a97fb87099e983c6d15f66df5841d69abbff9cf2
linyc74 <ylin@nycu.edu.tw>
2024-10-02 12:28:25 +0800

    add libffi

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,7 @@ from .io import IO
 from .controller import Controller
 
 
-VERSION = 'v1.0.0'
+VERSION = 'v1.0.1-beta'
 STARTING_MESSAGE = f'''\
 Somatic App {VERSION}
 College of Dentistry, National Yang Ming Chiao Tung University (NYCU), Taiwan
